4f80b43fcbb9ae82ba8efc09d1a8c4ca7d12a01c
zvadaadam <zvadaada@gmail.com>
2018-12-28 19:28:51 +0100

    strategy pattern for pagination

--- filabel/logic.py
+++ filabel/logic.py
@@ -4,8 +4,109 @@ import itertools
 import requests
 import aiohttp
 import asyncio
+import abc
 from urllib import parse
 
+
+class PaginationStrategy(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def paginated_get(self, url, params=None, headers=None, session=None):
+        pass
+
+
+class SyncPagination(PaginationStrategy):
+
+    def paginated_get(self, url, params=None, headers=None, session=None):
+        """"
+        If the request response can be paginated, it retrives the whole response.
+
+        :raise HTTPError: if during the request is raised
+
+        :param url: url for outgoing request
+
+        :param Optinal[dict[str, str] params: parameters for request
+
+        :rtype dict: json
+
+        :return: whole already paginated json reponse for given url and params
+        """
+
+        r = session.get(url, params=params)
+        r.raise_for_status()
+        json = r.json()
+
+        if 'next' in r.links and 'url' in r.links['next']:
+            json += self.paginated_get(r.links['next']['url'], params=params, session=session)
+
+        return json
+
+
+class AsyncPagination(PaginationStrategy):
+
+    def paginated_get(self, url, params=None, headers=None, session=None):
+
+        loop = asyncio.get_event_loop()
+
+        json = loop.run_until_complete(self._paginated_get(url, params))
+
+        loop.close()
+
+        return json
+
+    async def _paginated_get(self, url, params=None, headers=None, session=None):
+
+        session = aiohttp.ClientSession(headers=headers)
+        async with session:
+            future = asyncio.ensure_future(self._get_reponse(session, url, params))
+
+            json, links = await future
+
+            if links != None:
+                next_num = self._page_num_from_ulr(links[0]['url'])
+                last_num = self._page_num_from_ulr(links[1]['url'])
+
+                reponse_paginated = await asyncio.gather(
+                    *[self._get_reponse(session, self._url_with_page_num(links[0]['url'], page_num)) for page_num in
+                      range(next_num, last_num + 1)])
+
+                for reponse in reponse_paginated:
+                    json += reponse[0]
+
+            return json
+
+    async def _get_reponse(self, session, url, params=None):
+
+        async with session.get(url, params=params) as response:
+
+            future_json = await response.json()
+
+            links = None
+            if response.headers.get('Link'):
+                links = requests.utils.parse_header_links(response.headers.get('Link'))
+
+            return future_json, links
+
+    def _url_with_page_num(self, url, num):
+
+        parsed_url = parse.urlparse(url)
+
+        url_dict = parse.parse_qs(parsed_url.query)
+        url_dict['page'] = [num]
+
+        parsed_url = list(parsed_url)
+
+        parsed_url[4] = parse.urlencode(url_dict, doseq=True)
+
+        return parse.urlunparse(parsed_url)
+
+
+
+    def _page_num_from_ulr(self, url):
+
+        parsed_url = parse.urlparse(url)
+
+        return int(parse.parse_qs(parsed_url.query)['page'][0])
 
 
 class GitHub:
@@ -17,7 +118,7 @@ class GitHub:
     """
     API = 'https://api.github.com'
 
-    def __init__(self, token, session=None):
+    def __init__(self, token, strategy=None, session=None):
         """
         Initilizer for GitHub API wrapper.
 
@@ -25,6 +126,9 @@ class GitHub:
 
         :param Optinal[session] session: optional requests session
         """
+
+        self.strategy = strategy or SyncPagination()
+
         self.token = token
         self.session = session or requests.Session()
         self.session.headers = {'User-Agent': 'filabel'}
@@ -41,103 +145,6 @@ class GitHub:
         req.headers['Authorization'] = 'token ' + self.token
         return req
 
-    def async_session(self):
-
-        return aiohttp.ClientSession(headers={'User-Agent': 'filabel', 'Authorization': f'token {self.token}'})
-
-    async def _paginated_json_get_async(self, url, params=None):
-        """
-        """
-
-        session = self.async_session()
-        async with session:
-            future = asyncio.ensure_future(self._get_reponse(session, url, params))
-
-            json, links = await future
-
-            if links != None:
-                next_num = self.page_num_from_ulr(links[0]['url'])
-                last_num = self.page_num_from_ulr(links[1]['url'])
-
-                reponse_paginated = await asyncio.gather(*[self._get_reponse(session, self.url_with_page_num(links[0]['url'], page_num)) for page_num in range(next_num, last_num + 1)])
-
-                for reponse in reponse_paginated:
-                    json += reponse[0]
-
-            return json
-
-            #reponse_paginated = await asyncio.gather(*[self._get_reponse(session, url) for url in links])
-
-    def url_with_page_num(self, url, num):
-
-        parsed_url = parse.urlparse(url)
-
-        url_dict = parse.parse_qs(parsed_url.query)
-        url_dict['page'] = [num]
-
-        parsed_url = list(parsed_url)
-
-        parsed_url[4] = parse.urlencode(url_dict, doseq=True)
-
-        return parse.urlunparse(parsed_url)
-
-
-
-    def page_num_from_ulr(self, url):
-        """
-
-        :param url:
-        :return:
-        """
-        parsed_url = parse.urlparse(url)
-
-        return int(parse.parse_qs(parsed_url.query)['page'][0])
-
-
-    async def _get_reponse(self, session, url, params=None):
-        """
-
-        :param session:
-        :param url:
-        :param params:
-        :return:
-        """
-
-        async with session.get(url, params=params) as response:
-
-            future_json = await response.json()
-
-            links = None
-            if response.headers.get('Link'):
-                links = requests.utils.parse_header_links(response.headers.get('Link'))
-
-            return future_json, links
-
-
-
-    def _paginated_json_get(self, url, params=None):
-
-        """"
-        If the request response can be paginated, it retrives the whole response.
-
-        :raise HTTPError: if during the request is raised
-
-        :param url: url for outgoing request
-
-        :param Optinal[dict[str, str] params: parameters for request
-
-        :rtype dict: json
-
-        :return: whole already paginated json reponse for given url and params
-        """
-
-        r = self.session.get(url, params=params)
-        r.raise_for_status()
-        json = r.json()
-        if 'next' in r.links and 'url' in r.links['next']:
-            json += self._paginated_json_get(r.links['next']['url'], params)
-        return json
-
     def user(self):
         """
         Get current Github user authenticated by given token
@@ -146,7 +153,10 @@ class GitHub:
 
         :return: current user information
         """
-        return self._paginated_json_get(f'{self.API}/user')
+        #return self._paginated_json_get(f'{self.API}/user')
+
+        url = f'{self.API}/user'
+        return self.strategy.paginated_get(url, session=self.session)
 
     def pull_requests(self, owner, repo, state='open', base=None):
         """
@@ -171,15 +181,10 @@ class GitHub:
             params['base'] = base
         url = f'{self.API}/repos/{owner}/{repo}/pulls'
 
+        return self.strategy.paginated_get(url, params=params, session=self.session)
+
         #return self._paginated_json_get(url, params)
 
-        loop = asyncio.get_event_loop()
-
-        json = loop.run_until_complete(self._paginated_json_get_async(url, params))
-
-        loop.close()
-
-        return json
 
 
     def pr_files(self, owner, repo, number):
@@ -198,9 +203,9 @@ class GitHub:
         """
         url = f'{self.API}/repos/{owner}/{repo}/pulls/{number}/files'
 
-        #print(url)
+        return self.strategy.paginated_get(url, session=self.session)
 
-        return self._paginated_json_get(url)
+        #return self._paginated_json_get(url)
 
     def pr_filenames(self, owner, repo, number):
         """
@@ -421,7 +426,8 @@ if __name__ == "__main__":
     token = os.environ.get('GH_TOKEN', 'edd303d73b09c5e6914b5c881ee4b6b6bd8ff398')
     print(token)
 
-    github = GitHub(token)
+    github = GitHub(token, strategy=AsyncPagination())
+    #github = GitHub(token)
 
     owner = 'zvadaadam'
     repo = 'filabel-testrepo2'
